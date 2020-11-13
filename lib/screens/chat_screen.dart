4d323eb2971d2411a2a0e810b03c04f57c275274
matr0s <serhiy.osadchuk@gmail.com>
2020-11-13 22:06:33 +0200

    Firestore database added and Message sending

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -9,8 +10,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   User logedInUser;
+  String message;
 
   @override
   void initState() {
@@ -60,14 +63,19 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        message = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      //Implement send functionality.
+                    onPressed: () async {
+                      try {
+                        await _fireStore.collection('messages').add(
+                            {'test': message, 'sender': logedInUser.email});
+                      } catch (e) {
+                        print(e);
+                      }
                     },
                     child: Text(
                       'Send',
