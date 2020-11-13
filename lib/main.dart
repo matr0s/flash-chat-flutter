3c244a6b59f02853f139b9aacb180c1a31e2a08d
matr0s <serhiy.osadchuk@gmail.com>
2020-11-13 21:41:34 +0200

    Spinner for Login and Registration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
-import 'package:flash_chat/screens/login_screen.dart';
-import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
 
 import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
